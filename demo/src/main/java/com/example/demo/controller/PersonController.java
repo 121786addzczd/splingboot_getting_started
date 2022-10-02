71bda3026fb38b170862ff9b1b899fe2468b8825
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-10-02 18:03:16 +0900

    Modelファイルの作成

---
+++ demo/src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.models.Person;
+
+@Controller
+public class PersonController {
+    
+    @GetMapping("/")
+    public String index(@ModelAttribute Person person){
+        return "person/index";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Person person){
+        return "person/create";
+    }
+}
